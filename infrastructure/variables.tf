03e4d0f923be7b545a37a5426a5d5e89847ef31e
Ilich Briceno <ilich@escala.com>
2023-02-03 18:08:01 -0300

    backend tf s3

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,19 +1,3 @@
 variable "aws_region" {
   default = "us-east-2"
 }
-
-variable "lambda_function_name" {
-  default = "IGTIexecutaEMRaovivo"
-}
-
-variable "key_pair_name" {
-  default = "ib-igti-teste"
-}
-
-variable "airflow_subnet_id" {
-  default = "subnet-4cef5427"
-}
-
-variable "vpc_id" {
-  default = "vpc-d724b4bc"
-}
