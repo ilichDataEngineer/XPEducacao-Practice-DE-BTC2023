8334e93d57c08fcb51a671fd89f5e66d1eb0a4f2
Ilich Briceno <ilich@escala.com>
2023-02-05 15:55:09 -0300

    testing a generic JSON policy code for kinesis

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -76,9 +76,39 @@ resource "aws_iam_role_policy_attachment" "lambda_attach" {
   policy_arn = aws_iam_policy.lambda.arn
 }
 
+#############
+## KINESIS ##
+#############
 
+ resource "aws_iam_policy" "firehose" {
+   name        = "IGTIFirehosePolicy"
+   path        = "/"
+   description = "Provides write permissions to CloudWatch Logs and S3"
 
+   policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "s3:PutObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
 
+ resource "aws_iam_role_policy_attachment" "firehose_attach" {
+   role       = aws_iam_role.firehose_role.name
+   policy_arn = aws_iam_policy.firehose.arn
+ }
+ 
 ###############
 ## GLUE ROLE ##
 ###############
